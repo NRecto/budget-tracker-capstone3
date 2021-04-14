4bccc4a0831b3917077b731db19e11111780c936
Nicolas Miguel Recto <recto.nicolas1106@gmail.com>
2021-04-14 14:49:20 +0800

    add date range for line chart

--- components/LineCharts.js
+++ components/LineCharts.js
@@ -1,26 +1,43 @@
 import React, {useState, useEffect } from 'react';
 import { Line } from 'react-chartjs-2';
 import DatePicker from "react-datepicker";
+import moment from 'moment';
 
 
-// import moment from 'moment';
 
 export default function BarChart({rawData}) {
-    
+    let x = new Date();
     const [ amount, setAmount ] = useState([])
     const [balance,setBalance] = useState([]);
-    const [startDate, setStartDate] = useState(new Date());
-    const [month, setMonth] =useState()
+    const [startDate, setStartDate] = useState(x.setMonth(x.getMonth()-1));
+    const [endDate, setEndDate] = useState(new Date());
+    const [dataPerMonth, setDataPerMonth] =useState();
+    
+    const changedDateData = rawData.map( data => {
+        return ({
+            date: data.createdOn,
+            amount:data.amount,
+            type: data.type
+        })
+    })
+    const sortedData = changedDateData.sort( (a,b) => a.date < b.date ? -1 : 1).filter( data => {
+        return (moment(data.date).format('MM DD YYYY') >= moment(startDate).format('MM DD YYYY') && moment(data.date).format('MM DD YYYY') <= moment(endDate).format('MM DD YYYY'));
+    })
 
-
+    
+    
     useEffect( () => {
-        setAmount( rawData.map( data => {
+        // data on selected month
+        // setDataPerMonth( rawData.ma)
+        
+        // data amount
+        setAmount( sortedData.map( data => {
             return data.amount
         }) )
-
-            let total = 0;
-            const userBalance = rawData.map( data => {
-                
+        
+        let total = 0;
+        const userBalance = sortedData.map( data => {
+            
                 if(data.type === 'Income'){
                     total = total + data.amount
                 } else {
@@ -31,7 +48,7 @@ export default function BarChart({rawData}) {
             } )
             setBalance(userBalance)
 
-    }, [rawData]) 
+    }, [rawData, startDate, endDate]) 
 
     const data ={        
                     labels: amount,
@@ -55,18 +72,16 @@ export default function BarChart({rawData}) {
             radius: 10,
             interaction: {
               intersect: false,
-            }
-        }
-
+            },
+            
+        },
     }
-     
-    console.log(startDate);
+
     return (
         <React.Fragment>
             <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
+            <DatePicker selected={endDate} onChange={date => setEndDate(date)} />
             <Line data={data} config={config} />  
         </React.Fragment>
     )
 }
-
-// mvtalan@gmail.com
